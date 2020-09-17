b5b483d637cafa0753324aa91c40353c06f19551
EIivaras <zach00182@gmail.com>
2020-09-17 19:27:46 -0400

    Initialized service with some basic code

--- frontend/src/app/online-games.service.ts
+++ frontend/src/app/online-games.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OnlineGamesService {
+  readonly ROOT_URL;
 
-  constructor() { }
+  constructor (private http: HttpClient) {
+    this.ROOT_URL = "http://localhost:3000";
+  }
 }
