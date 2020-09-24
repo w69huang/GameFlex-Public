826c67baecdc6aea303ffa8ce9ecc8a00db5d58e
Wenxuan Huang <w69huang@uwaterloo.ca>
2020-09-24 19:23:07 -0400

    sign up componenets

---
+++ frontend/src/app/signup-login/signup-login.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SignupLoginComponent } from './signup-login.component';
+
+describe('SignupLoginComponent', () => {
+  let component: SignupLoginComponent;
+  let fixture: ComponentFixture<SignupLoginComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SignupLoginComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SignupLoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
