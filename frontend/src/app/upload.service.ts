162a2a8dd65f5921e2dc117671b7f6bc6e28c54d
Jackson <jbarr@uwaterloo.ca>
2020-09-27 13:11:15 -0400

    File uploads with GridFS working

--- frontend/src/app/upload.service.ts
+++ frontend/src/app/upload.service.ts
@@ -1,24 +1,44 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpEvent, HttpErrorResponse, HttpEventType } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { BehaviorSubject, Subject, Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
+export class UploadService {
+  SERVER_URL: string = "http://localhost:4000/";
+  constructor(private httpClient: HttpClient) { }
+
+  public upload(formData) {
+    return this.httpClient.post<any>(this.SERVER_URL, formData, {
+      reportProgress: true,
+      observe: 'events'
+    });
+  }
+}
+
 // export class UploadService {
-//   SERVER_URL: string = "https://file.io/";
+//   private fileList: string[] = new Array<string>();
+//   private fileList$: Subject<string[]> = new Subject<string[]>();
+//   private displayLoader$: Subject<boolean> = new BehaviorSubject<boolean>(false);
+
 //   constructor(private httpClient: HttpClient) { }
 
-//   public upload(formData) {
-//     return this.httpClient.post<any>(this.SERVER_URL, formData, {
-//       reportProgress: true,
-//       observe: 'events'
+//   public isLoading(): Observable<boolean> {
+//     return this.displayLoader$;
+//   }
+
+//   public upload(fileName: string, fileContent: string): void {
+//     this.displayLoader$.next(true);
+//     this.httpClient.put('/files', {name: fileName, content: fileContent})
+//     .pipe(finalize(() => this.displayLoader$.next(false)))
+//     .subscribe(res => {
+//       this.fileList.push(fileName);
+//       this.fileList$.next(this.fileList);
+//     }, error => {
+//       this.displayLoader$.next(false);
 //     });
 //   }
 // }
-
-export class UploadService {
-  constructor(private httpClient: HttpClient) { }
-
-  upload
-}
